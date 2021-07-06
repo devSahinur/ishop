c7f2387316ba45d1cbd543c37f8a01613c32f2d5
Sahinur Islam <infosahinur@gmail.com>
2021-07-07 01:16:56 +0600

    header problem fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
 import LoadingBar from "react-top-loading-bar";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Header from './components/Header/Header'
 import './App.css';
@@ -13,6 +14,12 @@ function App() {
         <AnimatePresence exitBeforeEnter>
           <AnimateSharedLayout>
             <Header/>
+            <Switch>
+              <Route path="/login">
+                {/* <UserPage type="login" /> */}
+              </Route>
+            </Switch>
+
           </AnimateSharedLayout>
         </AnimatePresence>
       </div>
