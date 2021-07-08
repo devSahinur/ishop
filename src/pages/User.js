3efd93b65a865ee1c04eb468fd3854b26c11535d
Sahinur Islam <infosahinur@gmail.com>
2021-07-08 13:06:05 +0600

    user page modified

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { motion } from "framer-motion";
 import Login from '../components/Login/Login'
+import Singup from '../components/Signup/Signup'
+import Welcome from '../components/Welcome/Welcome'
+import PasswordReset from '../components/PasswordReset/PasswordReset'
+import Profile from '../components/Profile/Profile'
 
 const User = ({ type }) => {
     return (
@@ -12,10 +16,10 @@ const User = ({ type }) => {
             // transition={pageTransition}
         >
             {type === 'login' && <Login/>}
-            {type === 'login' && <Login/>}
-            {type === 'login' && <Login/>}
-            {type === 'login' && <Login/>}
-            {type === 'login' && <Login/>}
+            {type === 'signup' && <Singup/>}
+            {type === "welcome" && <Welcome />}
+            {type === "passwordReset" && <PasswordReset />}
+            {type === "profile" && <Profile />}
         </motion.div>
     );
 };
